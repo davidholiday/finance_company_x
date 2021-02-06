523242407809a3fa4ca82fc5c7edc6d41e300fd8
snerd <you@example.com>
2021-02-06 14:08:17 -0700

    currentexchangeratebean tests done

--- src/test/java/io/holitek/CurrentExchangeRateBeanTest.java
+++ src/test/java/io/holitek/CurrentExchangeRateBeanTest.java
@@ -1,6 +1,7 @@
 package io.holitek;
 
 
+import com.jayway.jsonpath.JsonPath;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit5.CamelTestSupport;
 
@@ -11,13 +12,16 @@ import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 
 public class CurrentExchangeRateBeanTest extends CamelTestSupport {
     private static final Logger LOG = LoggerFactory.getLogger(CurrentExchangeRateBeanTest.class);
 
+    private String buildID;
     private String buildIdContents;
     private String dataFileContents;
+
 
     //
     // test setup and configuration
@@ -33,13 +37,15 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
                  .setLocation("classpath:application.test.properties");
 
 
-        buildIdContents = context().getPropertiesComponent()
-                                   .resolveProperty("buildID.contents")
-                                   .orElse("");
+        this.buildIdContents = context().getPropertiesComponent()
+                                        .resolveProperty("buildID.contents")
+                                        .orElse("");
 
-        dataFileContents = context().getPropertiesComponent()
-                                    .resolveProperty("dataFile.contents")
-                                    .orElse("");
+        this.buildID = JsonPath.read(this.buildIdContents, "$.buildID");
+
+        this.dataFileContents = context().getPropertiesComponent()
+                                         .resolveProperty("dataFile.contents")
+                                         .orElse("");
 
 
         context().getRegistry()
@@ -68,8 +74,8 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
     // tests
 
     @Test
-    @DisplayName("checks default exchange behavior")
-    public void testHappyPath() throws Exception {
+    @DisplayName("checks that bean in default state properly handles exchange messages ")
+    public void testExchangeHandlerHappyPath() throws Exception {
         getMockEndpoint("mock:result").expectedHeaderReceived(CurrentExchangeRateBean.BUILD_ID_HEADER_KEY, "");
         getMockEndpoint("mock:result").expectedBodiesReceived("{}");
         template.sendBody("direct:start", "");
@@ -82,12 +88,11 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
         CurrentExchangeRateBean currentExchangeRateBean = (
                 CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
 
-        String expectedBuildID = "foo";
-        boolean successFlag = currentExchangeRateBean.setBuildID(expectedBuildID);
+        boolean successFlag = currentExchangeRateBean.setBuildID(this.buildID);
         Assertions.assertTrue(successFlag);
 
         String actualBuildID = currentExchangeRateBean.getBuildID();
-        Assertions.assertEquals(expectedBuildID, actualBuildID, "we should get back what we put in!");
+        Assertions.assertEquals(this.buildID, actualBuildID, "we should get back what we put in!");
     }
 
     @Test
@@ -96,12 +101,11 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
 
         // setup
         //
-        String expectedBuildID = "foo";
         CurrentExchangeRateBean currentExchangeRateBean = (
                 CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
 
         // this way (vs junit way) because this is to validate programmer assumption that expected condition exists.
-        assert currentExchangeRateBean.setBuildID(expectedBuildID);
+        assert currentExchangeRateBean.setBuildID(this.buildID);
 
 
         // test
@@ -111,7 +115,7 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
 
         String actualBuildID = currentExchangeRateBean.getBuildID();
         Assertions.assertEquals(
-                expectedBuildID,
+                this.buildID,
                 actualBuildID,
                 "buildID value should not have changed on setBuildID fault"
         );
@@ -123,12 +127,10 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
 
         // setup
         //
-        String expectedBuildID = "foo";
         CurrentExchangeRateBean currentExchangeRateBean = (
                 CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
 
-        // this way (vs junit way) because this is to validate programmer assumption that expected condition exists.
-        assert currentExchangeRateBean.setBuildID(expectedBuildID);
+        assert currentExchangeRateBean.setBuildID(this.buildID);
 
 
         // test
@@ -138,7 +140,7 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
 
         String actualBuildID = currentExchangeRateBean.getBuildID();
         Assertions.assertEquals(
-                expectedBuildID,
+                this.buildID,
                 actualBuildID,
                 "buildID value should not have changed on setBuildID fault"
         );
@@ -181,7 +183,7 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
                 CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
 
         //
-        boolean successFlag = currentExchangeRateBean.setExchangeRateMap(dataFileContents);
+        boolean successFlag = currentExchangeRateBean.setExchangeRateMap(this.dataFileContents);
         Assertions.assertTrue(successFlag, "method expected to report operation was success");
 
         //
@@ -190,9 +192,59 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
         Assertions.assertEquals(expectedMapResults, actualMapResults, "what went in is not what came out");
 
         //
-        String expectedJsonResults = dataFileContents;
+        String expectedJsonResults = this.dataFileContents;
         String actualJsonResults = currentExchangeRateBean.getExchangeRatesAsJson();
         Assertions.assertEquals(expectedJsonResults, actualJsonResults);
+    }
+
+    @Test
+    @DisplayName("checks that a request for an exchange rate that doesn't exist returns empty optional")
+    public void testGetExchangeRateForNonExistentKey() {
+        CurrentExchangeRateBean currentExchangeRateBean = (
+                CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
+
+        assert currentExchangeRateBean.getExchangeRateMap().isEmpty();
+
+        Optional<Double> actualResult = currentExchangeRateBean.getExchangeRateFor("floopieDonkWagonTheThird");
+        Assertions.assertTrue(
+                actualResult.isEmpty(),
+                "bean should return empty optional for exchange rate key not present in the exchange rate map"
+        );
+    }
+
+    @Test
+    @DisplayName("checks that a request for an exchange rate that exists returns it wrapped in an optional")
+    public void testGetExchangeRateForExistentKey() {
+        CurrentExchangeRateBean currentExchangeRateBean = (
+                CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
+
+        // setup
+        //
+        String expectedKey = "CAD_USD";
+        Double expectedValue = 0.98;
+        assert currentExchangeRateBean.getExchangeRateMap().isEmpty();
+
+        assert currentExchangeRateBean.setExchangeRateMap(this.dataFileContents);
+
+        assert currentExchangeRateBean.getExchangeRateMap().containsKey(expectedKey);
+        assert currentExchangeRateBean.getExchangeRateMap().get(expectedKey).equals(expectedValue);
+
+        // test
+        //
+        Optional<Double> actualResult = currentExchangeRateBean.getExchangeRateFor(expectedKey);
+
+        Assertions.assertFalse(
+                actualResult.isEmpty(),
+                "method should not return an empty Optional in response to a request for something that " +
+                         "is in exchangeRate map"
+        );
+
+
+        Assertions.assertEquals(
+                expectedValue,
+                actualResult.get(),
+                "method returned an unexpected value"
+        );
     }
 
     @Test
@@ -206,12 +258,12 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
         assert currentExchangeRateBean.getExchangeRateMap().isEmpty();
         assert currentExchangeRateBean.getBuildID().isEmpty();
 
-        assert currentExchangeRateBean.setBuildID("foo");
-        assert currentExchangeRateBean.setExchangeRateMap(dataFileContents);
+        assert currentExchangeRateBean.setBuildID(this.buildID);
+        assert currentExchangeRateBean.setExchangeRateMap(this.dataFileContents);
 
-        assert currentExchangeRateBean.getBuildID().equals("foo");
+        assert currentExchangeRateBean.getBuildID().equals(this.buildID);
         assert currentExchangeRateBean.getExchangeRateMap().size() == 2;
-        assert currentExchangeRateBean.getExchangeRatesAsJson().equals(dataFileContents);
+        assert currentExchangeRateBean.getExchangeRatesAsJson().equals(this.dataFileContents);
 
         // test
         //
@@ -245,6 +297,138 @@ public class CurrentExchangeRateBeanTest extends CamelTestSupport {
         );
 
     }
+
+    @Test
+    @DisplayName("checks happy path for setExchangeRates")
+    public void testSetExchangeRates() {
+        CurrentExchangeRateBean currentExchangeRateBean = (
+                CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
+
+        // setup
+        //
+        assert currentExchangeRateBean.getExchangeRateMap().isEmpty();
+        assert currentExchangeRateBean.getBuildID().isEmpty();
+
+
+        // tests
+        //
+
+        //
+        boolean successFlag = currentExchangeRateBean.setExchangeRates(this.buildID, this.dataFileContents);
+        Assertions.assertTrue(successFlag, "method was expected to report successful completion of operation");
+
+        String actualBuildID = currentExchangeRateBean.getBuildID();
+        Assertions.assertEquals(
+                this.buildID,
+                actualBuildID,
+                "bean is returning an unexpected value for buildID"
+        );
+
+        //
+        Map<String, Double> expectedMap = Map.of("CAD_USD", 0.98, "FR_USD", 0.9);
+        Map<String, Double> actualMap = currentExchangeRateBean.getExchangeRateMap();
+        Assertions.assertEquals(
+                expectedMap,
+                actualMap,
+                "bean is returning an unexpected value for exchangeRateMap"
+        );
+
+        //
+        String actualJsonString = currentExchangeRateBean.getExchangeRatesAsJson();
+        Assertions.assertEquals(
+                this.dataFileContents,
+                actualJsonString,
+                "bean is returning an unexpected value for exchangeRateJson"
+        );
+
+    }
+
+    @Test
+    @DisplayName("checks that attempting to set exchange rates with a bad buildID results in a reset bean")
+    public void testSetExchangeRatesBadBuildId() {
+        CurrentExchangeRateBean currentExchangeRateBean = (
+                CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
+
+        // setup
+        //
+        assert currentExchangeRateBean.getExchangeRateMap().isEmpty();
+        assert currentExchangeRateBean.getBuildID().isEmpty();
+
+
+        // tests
+        //
+
+        //
+        boolean successFlag = currentExchangeRateBean.setExchangeRates("", this.dataFileContents);
+        Assertions.assertFalse(successFlag, "method was expected to report failed attempt at operation");
+
+        String actualBuildID = currentExchangeRateBean.getBuildID();
+        Assertions.assertEquals(
+                "",
+                actualBuildID,
+                "bean is returning an unexpected value for buildID"
+        );
+
+        //;
+        Map<String, Double> actualMap = currentExchangeRateBean.getExchangeRateMap();
+        Assertions.assertTrue(
+                actualMap.isEmpty(),
+                "bean is returning a populated exchangeRateMap when it shouldn't be"
+        );
+
+        //
+        String actualJsonString = currentExchangeRateBean.getExchangeRatesAsJson();
+        Assertions.assertEquals(
+                "{}",
+                actualJsonString,
+                "bean is returning an unexpected value for exchangeRateJson"
+        );
+
+    }
+
+    @Test
+    @DisplayName("checks that attempting to set exchange rates with a bad json data string results in a reset bean")
+    public void testSetExchangeRatesBadJsonString() {
+        CurrentExchangeRateBean currentExchangeRateBean = (
+                CurrentExchangeRateBean) context().getRegistry().lookupByName(CurrentExchangeRateBean.NAMESPACE_KEY);
+
+        // setup
+        //
+        assert currentExchangeRateBean.getExchangeRateMap().isEmpty();
+        assert currentExchangeRateBean.getBuildID().isEmpty();
+
+
+        // tests
+        //
+
+        //
+        boolean successFlag = currentExchangeRateBean.setExchangeRates(buildID, "");
+        Assertions.assertFalse(successFlag, "method was expected to report failed attempt at operation");
+
+        String actualBuildID = currentExchangeRateBean.getBuildID();
+        Assertions.assertEquals(
+                "",
+                actualBuildID,
+                "bean is returning an unexpected value for buildID"
+        );
+
+        //;
+        Map<String, Double> actualMap = currentExchangeRateBean.getExchangeRateMap();
+        Assertions.assertTrue(
+                actualMap.isEmpty(),
+                "bean is returning a populated exchangeRateMap when it shouldn't be"
+        );
+
+        //
+        String actualJsonString = currentExchangeRateBean.getExchangeRatesAsJson();
+        Assertions.assertEquals(
+                "{}",
+                actualJsonString,
+                "bean is returning an unexpected value for exchangeRateJson"
+        );
+
+    }
+
 }
 
 
