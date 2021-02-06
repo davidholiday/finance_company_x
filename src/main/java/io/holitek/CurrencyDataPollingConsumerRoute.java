1ea360779a1d8ea4d221c35d32802acb34345bc0
snerd <you@example.com>
2021-02-06 14:42:45 -0700

    got timer working

--- src/main/java/io/holitek/CurrencyDataPollingConsumerRoute.java
+++ src/main/java/io/holitek/CurrencyDataPollingConsumerRoute.java
@@ -4,9 +4,17 @@ import org.apache.camel.builder.RouteBuilder;
 
 public class CurrencyDataPollingConsumerRoute extends RouteBuilder {
 
+    private static final String POLLING_CONSUMER =
+            "{{route_from_template}}" + "{{polling_consumer_period}}";
+
+    private static final String BUILDFILE_URI =
+            "{{polling_consumer_URI_template}}" + "{{buildID_filename}}";
+
+
     @Override
     public void configure() throws Exception {
-        from("{{currency_data_polling_consumer_from}}")
+        from(POLLING_CONSUMER)
+                //.to(BUILDFILE_URI)
                 // grab current build ID
                 // choice on current build ID and build ID in bean
                     // if buildID delta - update currentexchangeratebean-er-ino
